36019e5b5cef36a288a0e5dd9a15325e093a3521
diak <diak@10.9.8.6>
2019-02-14 08:00:25 +0100

    föggvény áthelyezése a fgv.php-be

--- index.php
+++ index.php
@@ -7,23 +7,8 @@
     <body>
         Ez az index.php
         <?php
-            function sorsolas()
-            {
-                 $kihuzottak = 0;
-                    while($kihuzottak < 5)
-                    {
-                        $sorsoltszam = rand(1,90);
-                        if(!isset($tomb[$sorsoltszam]))
-                        {
-                            $tomb[$sorsoltszam] = 1;
-                            $kihuzottak++;
-                        }
-                    }
-                    return $tomb;
-                    //var_dump($tomb);
-            }
             $sorsolt=sorsolas();  
-            var_dump($tomb);
+            var_dump($sorsolt);
             //LOTTÓHÚZÁS
                    
                     echo "Ezek voltak a számok";
